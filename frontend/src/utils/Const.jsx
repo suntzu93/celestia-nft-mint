b53a1477b1abf082634453b38e235c37f1a5f221
suntzu93 <thanhlv93@gmail.com>
2023-04-01 06:59:24 +0700

    change network name

--- frontend/src/utils/Const.jsx
+++ frontend/src/utils/Const.jsx
@@ -1,8 +1,8 @@
 export const RPC_URL = "https://ethermintd-blockspacerace.suntzu.pro"
-export const CHAIN_NAME = "Ethermintd testnet"
+export const CHAIN_NAME = "Celestia testnet"
 export const CHAIN_ID = 9000
 export const CHAIN_ID_HEX = "0x2328" //9000
-export const NETWORK = "ethermint"
+export const NETWORK = "blockspacerace"
 export const SYMBOL = "ETH"
 export const CONTRACT = "0xda4cB4430e8f5df91723804f2C7163127161Ab75"
 export const FAUCET_API = "https://faucet-ethermintd.suntzu.pro/faucet"

865b39408328e33aee849551329cf5568b39cb0a
suntzu93 <thanhlv93@gmail.com>
2023-03-31 20:44:30 +0700

    update port to 1116

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 1113
+    strictPort: true,
+    port: 1116
   },
   plugins: [react()],
 })

3ef9792841674066308139f0bc7f5edb93e21eff
suntzu93 <thanhlv93@gmail.com>
2023-04-04 07:59:19 +0700

    update contracts

--- frontend/src/utils/Const.jsx
+++ frontend/src/utils/Const.jsx
@@ -4,8 +4,7 @@ export const CHAIN_ID = 9000
 export const CHAIN_ID_HEX = "0x2328" //9000
 export const NETWORK = "blockspacerace"
 export const SYMBOL = "ETH"
-export const NFT_CONTRACT = "0xAC42F1aA3a0950a933912140c8E45d2312a68a76"
-export const STAKING_CONTRACT = "0x2AC5235F9c02920DE162afa22fa57F3aC1D8536e"
-export const TOKEN_CONTRACT = "0xa5F62c35FD57619fB577f1b38E9F368c0E58eDec"
+export const NFT_CONTRACT = "0xCA939ccB4AEAF64FDbF9F3e532bE72Ec70813E2E"
+export const STAKING_CONTRACT = "0xD69540a4767A9f069bC3f3f70177b40F3a0096Db"
 
 export const FAUCET_API = "https://faucet-ethermintd.suntzu.pro/faucet"
